d3fe0858cb53c6fae3f36bfcbf634e90f134a351
padamrijalofficial <105359768+padamrijalofficial@users.noreply.github.com>
2022-07-25 19:20:40 +0545

    Add files via upload

---
+++ models/order.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require("../customerDatabase/database");
+
+const Order = sequelize.define("order", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  total: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+});
+
+module.exports =  Order;
